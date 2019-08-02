2eff5145ce876b29c3004dd797d23202a3a2c2bc
Balro <anyuzun@163.com>
2019-08-02 16:14:10 +0800

    no comment.

--- out/lark/apps/alert/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "alert"
-urlpatterns = [
-    path("ding", views.ding, name="ding"),
-    path("phone", views.phone, name="phone"),
-    path("mail", views.mail, name="mail"),
-]
--- out/lark/apps/mysite/views.py
+++
@@ -1,7 +0,0 @@
-from django.views.decorators.http import require_GET
-from django.http import HttpResponse
-
-
-@require_GET
-def status(request):
-    return HttpResponse("ok")
